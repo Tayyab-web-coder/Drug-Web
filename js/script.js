43cdde475aed6ffa8b64d3a4d519548fdad897e0
Tayyab-web-coder <tayyabirshad03@gmail.com>
2024-11-08 07:32:31 +0500

    add code

--- js/script.js
+++ js/script.js
@@ -12,17 +12,14 @@ hamburger.addEventListener('click', () => {
     }
 });
       
-// JavaScript to set the active class based on the current page
-document.querySelectorAll('nav ul li a').forEach(link => {
-    // Get the current URL and compare it to the href of each link
-    if (window.location.pathname === '/' && link.getAttribute('href') === './index.html') {
-        // Mark "Home" as active when at the root domain
-        link.classList.add('active');
-    } else if (link.href === window.location.href) {
-        // Mark the link as active if it matches the current URL
-        link.classList.add('active');
-    } else {
-        // Remove active from other links
-        link.classList.remove('active');
-    }
-});
+window.onload = function() {
+    const links = document.querySelectorAll('.nav-link');
+    links.forEach(link => {
+       if (link.href === window.location.href) {
+          link.classList.add('active');
+       } else {
+          link.classList.remove('active');
+       }
+    });
+ };
+ 
